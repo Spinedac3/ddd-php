66fef631adb24e76b8eb252d4a5df93bc5d0d12a
Samuel Pineda <sc0149gt@gmail.com>
2024-12-27 18:57:34 -0600

    :sparkles: Base Class for Exceptions

---
+++ src/Exceptions/GenericException.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Spineda\DddFoundation\Exceptions;
+
+use Exception;
+
+/**
+ * Base class for exceptions
+ *
+ * @package Spineda\DddFoundation
+ */
+class GenericException extends Exception
+{
+    /**
+     * @var string
+     */
+    protected $message = 'Error general';
+
+    /**
+     * @var int
+     */
+    protected $code = 500;
+
+    /**
+     * Exception constructor
+     *
+     * @param   string|null $message   Exception message
+     * @param   int|null    $code      Exception code
+     * @param   Exception|null $previous  Previous exception - for nesting
+     */
+    public function __construct($message = null, $code = null, $previous = null)
+    {
+        if (null === $message) {
+            $message = $this->message;
+        }
+
+        if (null === $code) {
+            $code = $this->code;
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
